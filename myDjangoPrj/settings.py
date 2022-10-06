6b1900954b9b343bfcfd1f640fd2d20d706c121e
MinseoShindor <tlsalstj01@duksung.ac.kr>
2022-10-06 12:43:56 +0900

    포스트 목록/상세 페이지에 부트스트랩 적용하기

--- myDjangoPrj/settings.py
+++ myDjangoPrj/settings.py
@@ -119,7 +119,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = os.path.join(BASE_DIR, '_media')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
 MEDIA_URL = '/media/' #url 경로
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media') #저장하는 폴더
