3a3dc8f3fb097ee7d2e5475ccde15531f79895f6
MinseoShindor <tlsalstj01@duksung.ac.kr>
2022-10-11 16:03:17 +0900

    아이콘 및 사진 설정과 후크 텍스트

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+import os
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
@@ -19,3 +21,10 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1] # a.text => a text (.을 기준으로 나눠짐) 그 중 마지막
