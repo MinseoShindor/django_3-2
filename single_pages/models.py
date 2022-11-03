f5b9974805ca54228eec7b243b53fdbda3e35d15
MinseoShindor <tlsalstj01@duksung.ac.kr>
2022-11-03 12:15:33 +0900

    다대일 관계

--- single_pages/models.py
+++ single_pages/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+import os
 
-# Create your models here.
+
+
+class App(models.Model):
+    title = models.CharField(max_length=30) #앱 제목
+    description = models.TextField() #앱 소개
+    logo_image = models.ImageField(upload_to='duksung/images/%Y/%m/%d/', blank=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    developer = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'[{self.pk}] : {self.developer} '
+
+    def get_absolute_url(self):
+        return f'/duksung/{self.pk}/'
